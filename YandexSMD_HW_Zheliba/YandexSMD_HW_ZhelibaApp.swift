4443a5c0fddb8ffe39110025191c41a00d9f4593
JessiePinkMann <egor.anoshin.03@mail.ru>
2024-06-21 13:30:34 +0200

    Initial commit

---
+++ YandexSMD_HW_Zheliba/YandexSMD_HW_ZhelibaApp.swift
@@ -0,0 +1,17 @@
+//
+//  YandexSMD_HW_ZhelibaApp.swift
+//  YandexSMD_HW_Zheliba
+//
+//  Created by Egor Anoshin on 21.06.2024.
+//
+
+import SwiftUI
+
+@main
+struct YandexSMD_HW_ZhelibaApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
